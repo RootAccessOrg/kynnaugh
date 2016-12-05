2d2f474865977c08f3836adb9d462bb370c29de2
Bob <bob+git@vulpin.com>
2016-12-05 17:46:09 +1100

    Add some plugin functions

---
+++ kynnaugh/Plugin.cs
@@ -0,0 +1,74 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kynnaugh
+{
+    public class Plugin
+    {
+        // based on / a port of https://github.com/allquixotic/kynnaugh-cc/blob/master/kynnaugh.cpp
+        const string PluginName = "kynnaugh";
+        const string PluginVersion = "1.0";
+        const int PluginApiVersion = 21;
+        const string PluginAuthor = "RootAccessOrg";
+        const string PluginDescription = "Speech recognition and text to speech for deaf users.";
+
+        /*********************************** Required functions ************************************/
+        /*
+         * If any of these required functions is not implemented, TS3 will refuse to load the plugin
+         */
+
+        /// <summary>
+        /// Unique name identifying this plugin
+        /// </summary>
+        /// <returns></returns>
+        [DllExport]
+        IntPtr ts3plugin_name()
+        {
+            return StringUtils.NativeUtf8FromString(PluginName);
+        }
+
+        /// <summary>
+        /// Plugin version
+        /// </summary>
+        /// <returns></returns>
+        [DllExport]
+        IntPtr ts3plugin_version()
+        {
+            return StringUtils.NativeUtf8FromString(PluginVersion);
+        }
+
+        /// <summary>
+        /// Plugin API version. Must be the same as the clients API major version, else the plugin fails to load.
+        /// </summary>
+        /// <returns></returns>
+        [DllExport]
+        int ts3plugin_apiVersion()
+        {
+            return PluginApiVersion;
+        }
+
+        /// <summary>
+        /// Plugin author
+        /// </summary>
+        /// <returns></returns>
+        [DllExport]
+        IntPtr ts3plugin_author()
+        {
+            return StringUtils.NativeUtf8FromString(PluginAuthor);
+        }
+
+        /// <summary>
+        /// Plugin description
+        /// </summary>
+        /// <returns></returns>
+        [DllExport]
+        IntPtr ts3plugin_description()
+        {
+            return StringUtils.NativeUtf8FromString(PluginDescription);
+        }
+    }
+}
---
+++ kynnaugh/StringUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kynnaugh
+{
+    class StringUtils
+    {
+        // By Hans Passant
+        // http://stackoverflow.com/a/10773988
+        public static IntPtr NativeUtf8FromString(string managedString)
+        {
+            int len = Encoding.UTF8.GetByteCount(managedString);
+            byte[] buffer = new byte[len + 1];
+            Encoding.UTF8.GetBytes(managedString, 0, managedString.Length, buffer, 0);
+            IntPtr nativeUtf8 = Marshal.AllocHGlobal(buffer.Length);
+            Marshal.Copy(buffer, 0, nativeUtf8, buffer.Length);
+            return nativeUtf8;
+        }
+
+        public static string StringFromNativeUtf8(IntPtr nativeUtf8)
+        {
+            int len = 0;
+            while (Marshal.ReadByte(nativeUtf8, len) != 0) ++len;
+            byte[] buffer = new byte[len];
+            Marshal.Copy(nativeUtf8, buffer, 0, buffer.Length);
+            return Encoding.UTF8.GetString(buffer);
+        }
+    }
+}
