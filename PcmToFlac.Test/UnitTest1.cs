0e2982f667af10382b5f47962bf5b55216287e8d
Bob <bob+git@vulpin.com>
2016-12-05 17:41:24 +1100

    Import PcmToFlac

---
+++ PcmToFlac.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace PcmToFlac.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMono()
+        {
+            File.WriteAllBytes(@"bensound-littleidea-48k-s16le-mono.flac", PcmToFlac.Convert(File.ReadAllBytes(@"bensound-littleidea-48k-s16le-mono.raw"), 1));
+        }
+
+        [TestMethod]
+        public void TestStereo()
+        {
+            File.WriteAllBytes(@"bensound-littleidea-48k-s16le-stereo.flac", PcmToFlac.Convert(File.ReadAllBytes(@"bensound-littleidea-48k-s16le-stereo.raw"), 2));
+        }
+    }
+}
